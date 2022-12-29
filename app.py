045d6a8ac2150b54220d54fba276f5b901160473
Devik <devik.nangia@ais.amity.edu>
2022-12-29 17:54:05 +0530

    fixed input output

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ app = Flask(__name__)
 
 url = "https://ai-chatbot.p.rapidapi.com/chat/free"
 
-
 headers = {
     "X-RapidAPI-Key": "653db342bemsh971c29bdad7f814p178e70jsn2b31538fd4ba",
     "X-RapidAPI-Host": "ai-chatbot.p.rapidapi.com"
@@ -22,42 +21,58 @@ headers2 = {
     "X-RapidAPI-Host": "dnaber-languagetool.p.rapidapi.com"
 }
 
-
 @app.route('/', methods=['POST', 'GET'])
 def index():
-
-
-    if request.method == "POST":
+    # global user_input
+    if request.method == 'POST':
+        # global user_input
         user_input = request.form.get("user_input")
+        print(user_input)
 
-    querystring = {"message": user_input, "uid": "user1"}
+        # user_input = request.form['user_input']
+    
+        querystring = {
+            "message": user_input,
+            "uid": "user1"
+        }
 
-    response = requests.request(
-        "GET", url, headers=headers, params=querystring).text
-    dic2 = json.loads(response)
-    print("Bot:", dic2.get("chatbot").get("response", "NULL"))  # 1
-    parser = GingerIt()
-    parser.parse(user_input)
-    a = parser.parse(user_input)
-    b = a.get("result")
+        response = requests.request("GET", url, headers = headers, params = querystring).text
 
-    payload = "language=en-US&text=" + user_input
+        dic2 = json.loads(response)
+        print("Bot:", dic2.get("chatbot").get("response", "NULL"))
 
-    response2 = requests.request(
-        "POST", url2, data=payload, headers=headers2).text
-    dic = json.loads(response2)
-    temp = dic.get("matches")
+        parser = GingerIt()
+        parser.parse(user_input)
 
-    grammatical_errors = []
+        a = parser.parse(user_input)
+        b = a.get("result")
 
-    for i in range(len(temp)):
-        c = temp[i]
-        print(c.get("message"))  # 2
-        grammatical_errors.append(c.get("message"))
+        payload = "language=en-US&text=" + user_input
 
-    if b == user_input:
-        print("Great job!! No corrections required.")  # 3
+        response2 = requests.request("POST", url2, data=payload, headers = headers2).text
+
+        dic = json.loads(response2)
+        temp = dic.get("matches")
+
+        grammatical_errors = []
+
+        for i in range(len(temp)):
+            c = temp[i]
+            print(c.get("message"))
+            grammatical_errors.append(c)
+
+        if b == user_input:
+            print("Great job!! No corrections required.")
+            computer_response3 = "Greate Job !! No corrections required."
+            return render_template('app.html',user_input = user_input, computer_response1 = "Bot: " + dic2.get("chatbot").get("response", "NULL"), computer_response2 = grammatical_errors, computer_response3 = computer_response3)
+        else:
+            print("Correct: ", b)
+            computer_response4 = "Correct: " + b
+            return render_template('app.html',user_input = user_input, computer_response1 = "Bot: " + dic2.get("chatbot").get("response", "NULL"), computer_response2 = grammatical_errors, computer_response4 = computer_response4 )
+
     else:
-        print("Correct:", b)  # 4
+        print("else statement executed")
+        return render_template('app.html')
 
-    return render_template('app.html', computer_response1="Bot: " + dic2.get("chatbot").get("response", "NULL"))
+if __name__=='__main__':
+    app.run(debug=True)
