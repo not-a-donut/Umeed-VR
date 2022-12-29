95eb586dfa6a6fe22d8fb2caa5e94a8e7ae98d4e
Devik <devik.nangia@ais.amity.edu>
2022-12-29 21:36:13 +0530

    modified python code

--- app.py
+++ app.py
@@ -21,58 +21,76 @@ headers2 = {
     "X-RapidAPI-Host": "dnaber-languagetool.p.rapidapi.com"
 }
 
+
 @app.route('/', methods=['POST', 'GET'])
 def index():
     # global user_input
-    if request.method == 'POST':
-        # global user_input
-        user_input = request.form.get("user_input")
-        print(user_input)
+    user_input = ""
+    error_count = 0
+    count = 0
+    while user_input != "exit":
+        if request.method == 'POST':
+            # global user_input
+            user_input = request.form.get("user_input")
+            print(user_input)
 
-        # user_input = request.form['user_input']
-    
-        querystring = {
-            "message": user_input,
-            "uid": "user1"
-        }
+            # user_input = request.form['user_input']
 
-        response = requests.request("GET", url, headers = headers, params = querystring).text
+            querystring = {
+                "message": user_input,
+                "uid": "user1"
+            }
 
-        dic2 = json.loads(response)
-        print("Bot:", dic2.get("chatbot").get("response", "NULL"))
+            response = requests.request(
+                "GET", url, headers=headers, params=querystring).text
 
-        parser = GingerIt()
-        parser.parse(user_input)
+            dic2 = json.loads(response)
+            print("Bot:", dic2.get("chatbot").get("response", "NULL"))
 
-        a = parser.parse(user_input)
-        b = a.get("result")
+            parser = GingerIt()
+            parser.parse(user_input)
 
-        payload = "language=en-US&text=" + user_input
+            a = parser.parse(user_input)
+            b = a.get("result")
 
-        response2 = requests.request("POST", url2, data=payload, headers = headers2).text
+            payload = "language=en-US&text=" + user_input
 
-        dic = json.loads(response2)
-        temp = dic.get("matches")
+            response2 = requests.request(
+                "POST", url2, data=payload, headers=headers2).text
 
-        grammatical_errors = []
+            dic = json.loads(response2)
+            temp = dic.get("matches")
 
-        for i in range(len(temp)):
-            c = temp[i]
-            print(c.get("message"))
-            grammatical_errors.append(c)
+            grammatical_errors = []
 
-        if b == user_input:
-            print("Great job!! No corrections required.")
-            computer_response3 = "Greate Job !! No corrections required."
-            return render_template('app.html',user_input = user_input, computer_response1 = "Bot: " + dic2.get("chatbot").get("response", "NULL"), computer_response2 = grammatical_errors, computer_response3 = computer_response3)
-        else:
-            print("Correct: ", b)
-            computer_response4 = "Correct: " + b
-            return render_template('app.html',user_input = user_input, computer_response1 = "Bot: " + dic2.get("chatbot").get("response", "NULL"), computer_response2 = grammatical_errors, computer_response4 = computer_response4 )
+            errors = []
+
+            for i in range(len(temp)):
+                c = temp[i]
+                print(c.get("message"))
+                grammatical_errors.append(c.get("message"))
+                errors.append(c.get("shortMessage"))
+
+            if b == user_input:
+                print("Great job!! No corrections required.")
+                computer_response3 = "Greate Job !! No corrections required."
+                return render_template('app.html', user_input=user_input, computer_response1="Bot: " + dic2.get("chatbot").get("response", "NULL"), computer_response2=grammatical_errors, computer_response3=computer_response3, error_count = error_count)
+            else:
+                print("Correct: ", b)
+                computer_response4 = "Correct: " + b
+                return render_template('app.html', user_input=user_input, computer_response1="Bot: " + dic2.get("chatbot").get("response", "NULL"), computer_response2=grammatical_errors, computer_response4=computer_response4, error_count = error_count)
+
+        for i in errors:
+            if i != "":
+                error_count += 1
+
+        count += 1
 
     else:
         print("else statement executed")
         return render_template('app.html')
+    
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     app.run(debug=True)
